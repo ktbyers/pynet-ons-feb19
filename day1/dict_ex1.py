d857f6fbd459675b0acb083d1ec66903ad4384ba
Kirk Byers <ktbyers@twb-tech.com>
2019-01-31 13:26:40 -0800

    Day1

--- day1/dict_ex1.py
+++ day1/dict_ex1.py
@@ -2,19 +2,19 @@
 from __future__ import print_function
 
 net_device = {
-    'ip_addr': '172.30.220.1',
-    'username': 'admin',
-    'password': 'some_pass',
-    'vendor': 'cisco',
-    'model': '3945',
+    "ip_addr": "172.30.220.1",
+    "username": "admin",
+    "password": "some_pass",
+    "vendor": "cisco",
+    "model": "3945",
 }
 
 print()
 for k, v in net_device.items():
     print(k, v)
 
-net_device['password'] = 'new_value'
-net_device['secret'] = 'some_secret'
+net_device["password"] = "new_value"
+net_device["secret"] = "some_secret"
 
-device_type = net_device.get('device_type', 'cisco_ios')
+device_type = net_device.get("device_type", "cisco_ios")
 print("\nDefault device type: {}\n".format(device_type))
