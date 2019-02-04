a35f927f128e48c5b6fd1d82b322eb299e9ad877
Kirk Byers <ktbyers@twb-tech.com>
2019-02-04 15:26:25 -0600

    Netmiko

---
+++ netmiko_example/netmiko_simple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from __future__ import print_function, unicode_literals
+from getpass import getpass
+from netmiko import ConnectHandler
+
+if __name__ == "__main__":
+
+    password = getpass("Enter password: ")
+    cisco1 = {
+        "device_type": "cisco_ios",
+        "ip": "184.105.247.70",
+        "username": "pyclass",
+        "password": password,
+    }
+
+    net_connect = ConnectHandler(**cisco1)
+    print(net_connect.find_prompt())
