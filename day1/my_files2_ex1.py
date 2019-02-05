3f3c566a2c2c787a027988920e0df651c56e233e
Greg Renner <renner.greg@mayo.edu>
2019-02-05 12:32:08 -0800

    Files exercises

---
+++ day1/my_files2_ex1.py
@@ -0,0 +1,12 @@
+
+output = """ This is a file
+that has multiple lines
+of text
+"""
+
+with open("my_files2_ex1.txt","w") as f:
+    f.write(output)
+ 
+Here is some more 
+text to add to 
+this file
---
+++ day1/my_files3_ex1.py
@@ -0,0 +1,9 @@
+
+output = """ 
+Here is some more 
+text to add to 
+this file
+"""
+
+with open("my_files2_ex1.txt","a") as f:
+   f.write(output) 
