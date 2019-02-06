be1b6bf835abf71f1ff041f436b0e47b42998306
Greg Renner <renner.greg@mayo.edu>
2019-02-06 14:28:44 -0800

    Day 2 func and regex

--- day1/my_func_ex1.py
+++ day1/my_func_ex1.py
@@ -1,18 +1,18 @@
 
 def my_func(x,y,z=20):
-    print(x + y + z)
+    return x + y + z
 
 my_func(2, 3, 4)
 
-my_func(x=10, y=20)
+print(my_func(x=10, y=20))
 
-my_func(5, y=10, z=25)
+print(my_func(5, y=10, z=25))
 
-my_func("X", "Y", "Z")
+print(my_func("X", "Y", "Z"))
 
 list1 = ['first', 'second', 'third']
 list2 = [1, 2, 3]
 list3 = ['uno', 'dos', 'tres']
 
-my_func(list1, list2, list3)
+print(my_func(list1, list2, list3))
 
---
+++ day1/my_regex_ex1.py
@@ -0,0 +1,27 @@
+import re
+
+with open("show_int_fa4.txt") as f:
+    show_int = f.read()
+
+print(show_int)
+
+input = re.search(r"(\d+) packets input, (\d+) bytes", show_int)
+output = re.search(r"(\d+) packets output, (\d+) bytes", show_int)
+
+print(input)
+
+print(input.group(0))
+
+packets_input = input.group(1)
+bytes_input = input.group(2)
+
+packets_output = output.group(1)
+bytes_output = output.group(2)
+
+print(packets_input)
+print(bytes_input)
+
+print("Packet input: {} Bytes input: {}".format(packets_input, bytes_input))
+
+print("Packet output: {} Bytes output: {}".format(packets_output, bytes_output))
+
