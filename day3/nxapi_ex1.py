631cb66be74b87fb827a75fdc192195dfbb57cc2
Kirk Byers <ktbyers@twb-tech.com>
2019-02-06 10:35:37 -0800

    NX API

---
+++ day3/nxapi_ex1.py
@@ -0,0 +1,22 @@
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from getpass import getpass
+from nxapi_plumbing import Device
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+device = Device(
+    api_format="jsonrpc",
+    host="nxos1.lasthop.io",
+    username="pyclass",
+    password=getpass(),
+    transport="https",
+    port=8443,
+    verify=False,
+)
+
+output = device.show("show version")
+serial_number = output["proc_board_id"]
+print()
+print(serial_number)
+print()
