8b020ebc3766aaa01fff46e1f6c921115e4e5da5
Kirk Byers <ktbyers@twb-tech.com>
2019-02-06 08:19:45 -0800

    Updates to repository

--- day1/files_ex1.py
+++ day1/files_ex1.py
@@ -1,38 +1,9 @@
 #!/usr/bin/env python
-from __future__ import print_function
 
 # READ ####
 f = open("my_file.txt")
-print("\nLoop directly over file")
-print("-" * 60)
-for line in f:
-    print(line.strip())
-print("-" * 60)
-
-f.seek(0)
-my_content = f.readlines()
-print("\nUse readlines method")
-print("-" * 60)
-for line in my_content:
-    print(line.strip())
-print("-" * 60)
-
-f.seek(0)
 my_content = f.read()
-print("\nUse read + splitlines")
-print("-" * 60)
-for line in my_content.splitlines():
-    print(line)
-print("-" * 60)
-f.close()
-
-with open("my_file.txt") as f:
-    print("\nUse with and loop over file")
-    print("-" * 60)
-    for line in f:
-        print(line.strip())
-    print("-" * 60)
-
+print(my_content)
 
 # WRITE ####
 print("\nWriting file.")
