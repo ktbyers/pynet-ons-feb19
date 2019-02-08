6efb9ad80414708df969a384ea93f6c8460543ab
Kirk Byers <ktbyers@twb-tech.com>
2019-02-07 17:11:38 -0800

    Jinja2 updates

---
+++ day4/napalm_ex2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from pprint import pprint as pp
+
+from napalm_base import get_network_driver
+from my_devices_na import juniper1, juniper2, cisco_rtr1
+
+def main():
+    for a_device in (juniper1, juniper2, cisco_rtr1):
+        device_type = a_device.pop('device_type')
+        driver = get_network_driver(device_type)
+        device = driver(**a_device)
+
+        print()
+        print(">>>Device open")
+        device.open()
+
+        print("-" * 50)
+        device_facts = device.get_lldp_neighbors()
+        pp(device_facts)
+    print()
+
+if __name__ == "__main__":
+    main()
