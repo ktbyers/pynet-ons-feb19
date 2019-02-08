663c6912259f2dd1e7b2ee52f582e8f8f4330591
Kirk Byers <ktbyers@twb-tech.com>
2019-02-07 16:34:35 -0800

    napalm

--- napalm_example/test_merge_nxos.py
+++ napalm_example/test_merge_nxos.py
@@ -5,22 +5,23 @@ from napalm import get_network_driver
 
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
- 
+
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
 
 def hit_enter():
     input("Hit enter to continue: ")
 
 
 if __name__ == "__main__":
-    host = 'nxos1.lasthop.io'
-    username = 'pyclass'
+    host = "nxos1.lasthop.io"
+    username = "pyclass"
     password = getpass()
     optional_args = {"port": 8443}
-    
-    driver = get_network_driver('nxos')
+
+    driver = get_network_driver("nxos")
     device = driver(host, username, password, optional_args=optional_args)
-    
+
     print()
     print(">>>Test device open")
     device.open()
@@ -28,7 +29,7 @@ if __name__ == "__main__":
 
     print()
     print(">>>Load config change (merge) - no commit")
-    device.load_merge_candidate(filename='nxos-merge.conf')
+    device.load_merge_candidate(filename="nxos-merge.conf")
     print(device.compare_config())
     hit_enter()
 
@@ -42,12 +43,12 @@ if __name__ == "__main__":
 
     print()
     print(">>>Load config change (merge) - commit")
-    device.load_merge_candidate(filename='nxos-merge.conf')
+    device.load_merge_candidate(filename="nxos-merge.conf")
     # print(device.compare_config())
     device.commit_config()
     hit_enter()
 
-#print()
-#print(">>>Rollback")
-#device.rollback()
-#hit_enter()
+    # print()
+    # print(">>>Rollback")
+    # device.rollback()
+    # hit_enter()
