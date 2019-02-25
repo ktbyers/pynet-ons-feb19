1617f8548afd1eae22a4d6808bfd4dbadd5297b7
Kirk Byers <ktbyers@twb-tech.com>
2019-02-25 12:43:30 -0800

    Add nornir templating

--- nornir/netmiko_template.py
+++ nornir/netmiko_template.py
@@ -4,15 +4,20 @@ from nornir.core.filter import F
 from nornir_utilities import nornir_set_creds, std_print
 from nornir.plugins.tasks.text import template_file
 
+import ipdb
 
 def test_task(task):
+    ipdb.set_trace()
+    group_name = task.host.groups[0]  
+    group = task.nornir.inventory.groups[group_name]
     result = task.host["template_config"] = task.run(task=template_file,
-                                            template="router.j2", path="")
+                                            template="router.j2", path="", group=group)
     return result
 
 
 def main():
     norn = InitNornir(config_file="nornir.yaml")
+    ipdb.set_trace()
     f = F(groups__contains="juniper")
     norn = norn.filter(f)
     result = norn.run(test_task, num_workers=1)
